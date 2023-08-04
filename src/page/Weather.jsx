820ae6ed235dfeb4c1941ae27ac944ed484a2b0d
eunjye <eunjh1220@naver.com>
2023-08-05 00:19:14 +0900

    start weather

---
+++ src/page/Weather.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+
+// https://api.openweathermap.org/data/2.5/weather?q=London&appid={dcd670f0c25b6cfb68061cc5dea79335}
+
+
+const Weather = () => {
+    return (
+        <div>
+            
+        </div>
+    );
+};
+
+export default Weather;
